baa2368c77f35f4fe1b1a8f0dfd34e5940f79ab6
Francois Landry <efl7126@umoncton.ca>
2023-01-24 20:06:04 -0500

    Added to main.py the public functions that can be called in jaad_data

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from jaad_data import JAAD
+
+if __name__ == "__main__":
+    data_path = os.path.dirname(os.path.realpath(__file__))
+    jaad = JAAD(data_path)
+
+    # Available functions in jaad object
+    #db = jaad.generate_database()
+    #jaad.get_data_stats()
+    #jaad.balance_samples_count()
+    #jaad.get_detection_data()
+    sequence = jaad.generate_data_trajectory_sequence("train")
